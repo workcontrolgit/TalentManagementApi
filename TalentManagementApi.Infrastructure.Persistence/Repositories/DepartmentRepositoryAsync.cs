368f335694095406c53b32cca0beb75185471f2e
Fuji <fuji.nguyen@workcontrol.com>
2024-01-15 22:01:38 -0500

    Add department repository

---
+++ TalentManagementApi.Infrastructure.Persistence/Repositories/DepartmentRepositoryAsync.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TalentManagementApi.Application.Interfaces.Repositories;
+using TalentManagementApi.Domain.Entities;
+using TalentManagementApi.Infrastructure.Persistence.Contexts;
+using TalentManagementApi.Infrastructure.Persistence.Repository;
+
+namespace TalentManagementApi.Infrastructure.Persistence.Repositories
+{
+    public class DepartmentRepositoryAsync : GenericRepositoryAsync<Department>, IDepartmentRepositoryAsync
+    {
+        private readonly DbSet<Department> _repository;
+
+        public DepartmentRepositoryAsync(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _repository = dbContext.Set<Department>();
+        }
+    }
+}
