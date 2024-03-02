db41aa278e32231f9e69d0c8f1ae478d36eb5910
Fuji <fuji.nguyen@workcontrol.com>
2024-03-02 16:28:08 -0500

    Add ElasticSearch setting and registration

--- TalentManagementApi.Infrastructure.Shared/ServiceRegistration.cs
+++ TalentManagementApi.Infrastructure.Shared/ServiceRegistration.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Nest;
+using System;
 using TalentManagementApi.Application.Interfaces;
 using TalentManagementApi.Domain.Settings;
 using TalentManagementApi.Infrastructure.Shared.Services;
@@ -14,6 +16,41 @@ namespace TalentManagementApi.Infrastructure.Shared
             services.AddTransient<IDateTimeService, DateTimeService>();
             services.AddTransient<IEmailService, EmailService>();
             services.AddTransient<IMockService, MockService>();
+        }
+
+        public static void AddElasticsearch(this IServiceCollection services, IConfiguration _config)
+        {
+            var settings = new ConnectionSettings(new Uri(_config["ElasticsearchSettings:uri"]));
+
+            var defaultIndex = _config["ElasticsearchSettings:defaultIndex"];
+
+            if (!string.IsNullOrEmpty(defaultIndex))
+                settings = settings.DefaultIndex(defaultIndex);
+
+            // The authentication options below are set if you have non-null/empty
+            // settings in the _config.  These are just samples -- there are
+            // other authentication methods available.
+            var apiKeyId = _config["ElasticsearchSettings:apiKeyId"];
+            var apiKey = _config["ElasticsearchSettings:apiKey"];
+
+            if (!string.IsNullOrEmpty(apiKeyId) && !string.IsNullOrEmpty(apiKey))
+            {
+                settings = settings.ApiKeyAuthentication(apiKeyId, apiKey);
+            }
+            else
+            {
+                var basicAuthUser = _config["ElasticsearchSettings:basicAuthUser"];
+                var basicAuthPassword = _config["ElasticsearchSettings:basicAuthPassword"];
+
+                if (!string.IsNullOrEmpty(basicAuthUser) && !string.IsNullOrEmpty(basicAuthPassword))
+                    settings = settings.BasicAuthentication(basicAuthUser, basicAuthPassword);
+            }
+
+            var client = new ElasticClient(settings);
+
+            // ElasticClient is thread-safe
+            // See https://www.elastic.co/guide/en/elasticsearch/client/net-api/current/lifetimes.html
+            services.AddSingleton<IElasticClient>(client);
         }
     }
 }
