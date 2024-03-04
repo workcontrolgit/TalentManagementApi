10208a72d5638b4b6854d59b75c8f467952d5128
Fuji <fuji.nguyen@workcontrol.com>
2024-03-03 22:27:43 -0500

    Fix prompt code

--- TalentManagementApi.WebApi/Controllers/v1/PromptController.cs
+++ TalentManagementApi.WebApi/Controllers/v1/PromptController.cs
@@ -1,19 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using TalentManagementApi.Application.Features.Prompts.Queries.GetPrompts;
-using TalentManagementApi.Application.Interfaces;
 
 namespace TalentManagementApi.WebApi.Controllers.v1
 {
     public class PromptController : BaseApiController
     {
-        private readonly IPromptService _promptService;
-
-        public PromptController(IPromptService promptService)
-        {
-            _promptService = promptService;
-        }
-
         [HttpGet]
         public async Task<IActionResult> CreateChatCompletionAsync([FromQuery] string prompt)
         {
